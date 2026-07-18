7220c043623372b8518bb3af29d16747e264c13e
j4is-hka <ulanak194@gmail.com>
2026-07-18 22:28:18 +0300

    Update arduino-traffic-light.cpp

--- src/arduino-traffic-light.cpp
+++ src/arduino-traffic-light.cpp
@@ -4,47 +4,44 @@
 bool started = false;
 
 void setup() {
-  pinMode(2, INPUT);
-  pinMode(5, OUTPUT);  // Красный
-  pinMode(7, OUTPUT);  // Желтый
-  pinMode(10, OUTPUT);  // Зеленый
+  pinMode(2, INPUT);   // внешний pull-down резистор уже есть
+  pinMode(5, OUTPUT);  // Красный
+  pinMode(7, OUTPUT);  // Желтый
+  pinMode(10, OUTPUT); // Зеленый
 }
 
 void loop() {
+  // Ловим нажатие (HIGH, потому что кнопка замыкает на 5V)
+  if (digitalRead(2) == HIGH) {
+    started = true;
+  }
 
-    if (digitalRead(2) == HIGH)
-    {
-    started = true;
-    }
+  if (!started) {
+    return;
+  }
 
-  // Пока кнопку ни разу не нажали - ничего не делаем
-    if (!started)
-    {
-    return;
-    } // Кнопка нажата
+  // 1. Красный 4 сек
+  digitalWrite(5, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(10, LOW);
+  delay(4000);
 
-    {
-        digitalWrite(5, HIGH); // Загорается красный
-        delay(4000);
-        for (int i = 0; i < 3; i++) // Загорается мигающий желтый
-        {
-            digitalWrite(7, HIGH);
-            delay(300);
-            digitalWrite(7, LOW);
-            delay(300);
-        }
-        digitalWrite(5, LOW); // Красный погас, желтый тоже
-        digitalWrite(10, HIGH); // Загорается зеленый
-        delay(4000);
-        for (int i = 0; i < 4; i++)
-        {
-            digitalWrite(10, HIGH);
-            delay(300);
-            digitalWrite(10, LOW);
-            delay(300);
-        }
-        digitalWrite(7, HIGH);
-        delay(2000);
-        digitalWrite(7, LOW);
-    }
+  // 2. Зелёный 4 сек
+  digitalWrite(5, LOW);
+  digitalWrite(10, HIGH);
+  delay(4000);
+
+  // 3. Зелёный мигает 4 раза
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(10, LOW);
+    delay(300);
+    digitalWrite(10, HIGH);
+    delay(300);
+  }
+  digitalWrite(10, LOW); // выключаем зелёный
+
+  // 4. Жёлтый 2 сек
+  digitalWrite(7, HIGH);
+  delay(2000);
+  digitalWrite(7, LOW);
 }
